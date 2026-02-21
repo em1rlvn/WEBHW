7c35b99f83ae14eb1a00a7bf8dd6e6fd188a43f3
em1rlvn <emirlan.omorbekov@alatoo.edu.kg>
2026-02-21 17:18:58 +0300

    fix base again

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Weather-Website-main/', 
+  build: {
+    outDir: 'dist',                 
+    assetsDir: 'assets',
+    sourcemap: false,               
+  },
 })
